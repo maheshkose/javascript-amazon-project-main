a9c021d92a7af0b4f6114ee83ccb321ec07cbcce
mahesh kose <ms2kose@gmail.com>
2025-05-05 16:53:49 +0530

    save data and genrate html for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,2 +1,4 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js"; 
+import { renderPaymentSummary } from "./checkout/paymentsummary.js";
 renderOrderSummary();
+renderPaymentSummary();
